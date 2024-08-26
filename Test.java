d7068cf365c3486c763aecdb33f2449b2d34ee98
janeejoyful <janeeyeak@gmail.com>
2024-08-26 10:05:13 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+// Test
+
+public class Test
+{
+   public static void main(String[] args)
+   {
+      System.out.println();
+      System.out.println("Measure today's knowledge by yesterday's confusion.");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("#\t#####\f\f#####\t#####\f\f#####\f\f#\f\f#   #\f\f#####\f\f#####");
+      System.out.println("#\t#   #\f\f#   #\t#   #\f\f#   #\f\f#\f\f##  #\f\f  #\f\f  #");
+      System.out.println("#\t#   #\f\f#   #\t#####\f\f#   #\f\f#\f\f# # #\f\f  #\f\f  #####");
+      System.out.println("#\t#   #\f\f#   #\t#    \f\f#   #\f\f#\f\f#  ##\f\f  #\f\f      #");
+      System.out.println("#\t#####\f\f#####\t#    \f\f#####\f\f#\f\f#   #\f\f  #\f\f  #####");
+   }
+}
